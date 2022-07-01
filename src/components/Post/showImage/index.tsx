1d4c674b71249e495addd3c95f9e777f39ababd9
Subash-G <subashg@mieupro.com>
2022-07-01 17:50:24 +0530

    Image Component

--- src/components/Post/showImage/index.tsx
+++ src/components/Post/showImage/index.tsx
@@ -1,23 +1,47 @@
-import {View, StyleSheet, Dimensions} from 'react-native';
+import {View, StyleSheet, Dimensions, TouchableOpacity} from 'react-native';
 import FastImage from 'react-native-fast-image';
 import React from 'react';
-const {width} = Dimensions.get('screen');
+import DoubleClick from 'react-native-double-tap';
 
-const PhotoShower = ({album}) => {
-  console.log(album[0].url);
+type ImageProps = {
+  album: {url: string; formats: any}[];
+  likePost: boolean;
+  setLikePost: Function;
+};
+
+const PhotoShower = ({album, likePost, setLikePost}: ImageProps) => {
   return (
-    <View style={[styles.container, {width: width}]}>
-      <FastImage style={styles.photo} source={{uri: album[0].url}} />
+    <View
+      style={[
+        styles.container,
+        {height: album[0]['formats']['small']['height']},
+      ]}>
+      <DoubleClick
+        doubleTap={() => {
+          setLikePost(!likePost);
+        }}
+        delay={200}>
+        <FastImage
+          style={[
+            styles.photo,
+            {height: album[0]['formats']['small']['height']},
+          ]}
+          source={{uri: album[0].url}}
+          resizeMode="contain"
+        />
+      </DoubleClick>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    height: 300,
+    width: '100%',
+    flex: 1,
   },
   photo: {
-    height: 300,
+    borderTopRightRadius: 30,
+    borderTopLeftRadius: 30,
   },
 });
 
